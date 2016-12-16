543b58cf5b34d53e6660f55a45b0c7093105f9b8
Niel <nielmce@gmail.com>
2016-12-16 10:19:15 +0000

    Add standard Flask src & href. Add procfile and remote host python code

--- school_donations_.py
+++ school_donations_.py
@@ -4,12 +4,14 @@ from pymongo import MongoClient
 import json
 from bson import json_util
 from bson.json_util import dumps
+import os
 
 app = Flask(__name__)
 
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
-DBS_NAME = 'donorsUSA'
+DBS_NAME = os.getenv('MONGO_DB_NAME','donorsUSA')
+MONGO_URI = os.getenv('MONGODB_URI')
 COLLECTION_NAME = 'projects'
 FIELDS = {'funding_status': True, 'school_state': True, 'resource_type': True, 'poverty_level': True,
           'date_posted': True, 'total_donations': True, '_id': False}
@@ -23,6 +25,8 @@ def index():
 @app.route("/donorsUS/projects")
 def donor_projects():
     connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    # connection = MongoClient(MONGO_URI)
+#This connection is required when hosted using a remote mongo db.
     collection = connection[DBS_NAME][COLLECTION_NAME]
     projects = collection.find(projection=FIELDS, limit=55000)
     json_projects = []
